dfb9e5e304bfcad60b34b5445e14b7447035c683
Daniel Deng <ddeng5@uwo.ca>
2016-12-01 16:14:00 -0500

    Customer can now attend a showing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, json, jsonify
+from flask import Flask, render_template, request, json, jsonify, flash, session
 from datetime import datetime
 from flaskext.mysql import MySQL
 app = Flask(__name__)
@@ -10,6 +10,7 @@ app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'MovieTheatre'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['SECRET_KEY'] = 'Secret String'
 mysql.init_app(app)
 
 
@@ -25,7 +26,8 @@ def main():
 def customerSearch():
 
 #query for genres
-    cursor = mysql.connect().cursor()
+    cxn = mysql.connect()
+    cursor = cxn.cursor()
     cursor.execute("SELECT DISTINCT Genre FROM Genre")
     genres = cursor.fetchall()
 
@@ -58,9 +60,31 @@ def customerSearch():
         returnShowing.append(i)
 
 
+    selName = str(request.form.get('selectedName'))
+    selShowing = str(request.form.get('selectedShowing'))
+    temp = selShowing.split()
 
 
+    print selName
 
+    if request.method == 'POST':
+        firstName = selName.split()
+        dateTime = temp[0] + " " + temp[1]
+
+        cursor.execute('''SELECT Customer.idCustomer FROM Customer WHERE Customer.FirstName=%s ''', (firstName[0]))
+        customerId = cursor.fetchone()
+        finalCustomerID = int(customerId[0])
+
+        cursor.execute('''SELECT Showing.idShowing FROM Showing INNER JOIN Movie ON Showing.Movie_idMovie=Movie.idMovie WHERE Showing.ShowingDateTime=%s''', (dateTime))
+        movieId = cursor.fetchone()
+        finalMovieId = int(movieId[0])
+
+        print finalMovieId
+        print finalCustomerID
+
+        cursor.execute("INSERT INTO Attend (Customer_idCustomer, Showing_idShowing) VALUES (%s, %s)", (finalCustomerID, finalMovieId))
+        cxn.commit()
+        flash(selName + " successfully booked the " + temp[2] + " showing at " + temp[1] + " on " + temp[0])
 
 
     return render_template("customer.html", genre=returnGenre, date=returnDate, name=returnName, showing=returnShowing)
@@ -73,10 +97,13 @@ def customerSearch():
 
 @app.route("/customer/selectShowing", methods=['POST','GET'])
 def selectShowing():
+
+    cxn = mysql.connect()
+    cursor = cxn.cursor()
     selName = str(request.form.get('selectedName'))
     selShowing = str(request.form.get('selectedShowing'))
 
-    cursor.execute('''SELECT Customer.idCustomer FROM Customer WHERE Customer.FirstName=%s ''', ('Will'))
+    cursor.execute('''SELECT Customer.idCustomer FROM Customer WHERE (Customer.FirstName + " " + Customer.LastName)=%s ''', ('selName'))
     customerId=cursor.fetchone()
 
 
